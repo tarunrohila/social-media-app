09816de49f037994a5772143c56d3b41538bb6e6
tarunrohila <tarunrohila@gmail.com>
2021-07-19 01:33:54 +0530

    added junit test cases files

---
+++ src/test/java/com/rohila/api/controller/UserControllerTest.java
@@ -0,0 +1,30 @@
+package com.rohila.api.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void userAction() {
+    }
+
+    @Test
+    void retrieveNewsfeeds() {
+    }
+
+    @Test
+    void createUser() {
+    }
+}
---
+++ src/test/java/com/rohila/api/gateway/impl/UserGatewayImplTest.java
@@ -0,0 +1,34 @@
+package com.rohila.api.gateway.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserGatewayImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void follow() {
+    }
+
+    @Test
+    void unfollow() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void retrieveUser() {
+    }
+}

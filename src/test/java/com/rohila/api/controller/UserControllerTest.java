8165012d302a96c749d58803ac91b6b29f3376c6
tarunrohila <tarunrohila@gmail.com>
2021-07-19 09:26:24 +0530

    updated test

--- src/test/java/com/rohila/api/controller/UserControllerTest.java
+++ src/test/java/com/rohila/api/controller/UserControllerTest.java
@@ -1,30 +1,89 @@
 package com.rohila.api.controller;
 
+import com.rohila.api.bean.Resource;
+import com.rohila.api.dto.Post;
+import com.rohila.api.helper.TestHelper;
+import com.rohila.api.response.Response;
+import com.rohila.api.service.PostService;
+import com.rohila.api.service.UserService;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(MockitoJUnitRunner.class)
 class UserControllerTest {
+
+    @InjectMocks
+    UserController userController;
+
+    @Mock
+    UserService userService;
 
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    void userAction() {
+    void testUserActionFollow() {
+        Mockito.when(userService.userAction(Mockito.any(String.class),Mockito.any(Long.class),Mockito.any(Long.class))).thenReturn(Response.assemble().build(HttpStatus.OK, new Resource<String>("id", "users", "success")));
+        Response response = userController.userAction("follows", new Long(1), new Long(4));
+        Assert.assertNotNull(response);
     }
 
     @Test
-    void retrieveNewsfeeds() {
+    void testUserActionFollowWhenError() {
+        Mockito.when(userService.userAction(Mockito.any(String.class),Mockito.any(Long.class),Mockito.any(Long.class))).thenReturn(null);
+        Response response = userController.userAction("follows", new Long(1), new Long(4));
+        Assert.assertNull(response);
     }
 
     @Test
-    void createUser() {
+    void testUserActionUnFollow() {
+        Mockito.when(userService.userAction(Mockito.any(String.class),Mockito.any(Long.class),Mockito.any(Long.class))).thenReturn(Response.assemble().build(HttpStatus.OK, new Resource<String>("id", "users", "success")));
+        Response response = userController.userAction("unfollows", new Long(1), new Long(4));
+        Assert.assertNotNull(response);
     }
+
+    @Test
+    void testUserActionUnFollowWhenError() {
+        Mockito.when(userService.userAction(Mockito.any(String.class),Mockito.any(Long.class),Mockito.any(Long.class))).thenReturn(null);
+        Response response = userController.userAction("unfollows", new Long(1), new Long(4));
+        Assert.assertNull(response);
+    }
+
+    @Test
+    void testRetrieveNewsfeeds() {
+        List<Long> ids = new ArrayList<>();
+        ids.add(new Long(2));
+        ids.add(new Long(3));
+        Mockito.when(userService.retrieveNewsfeeds(Mockito.any(Long.class))).thenReturn(Response.assemble().build(new Resource<List<Long>>("id", "posts", ids)));
+        Response response = userService.retrieveNewsfeeds(new Long(2));
+        Assert.assertNotNull(response);
+    }
+
+    @Test
+    void testRetrieveNewsfeedsWhenError() {
+        List<Long> ids = new ArrayList<>();
+        ids.add(new Long(2));
+        ids.add(new Long(3));
+        Mockito.when(userService.retrieveNewsfeeds(Mockito.any(Long.class))).thenReturn(null);
+        Response response = userService.retrieveNewsfeeds(new Long(2));
+        Assert.assertNull(response);
+    }
+
 }
--- src/test/java/com/rohila/api/service/impl/UserServiceImplTest.java
+++ src/test/java/com/rohila/api/service/impl/UserServiceImplTest.java
@@ -1,30 +1,53 @@
 package com.rohila.api.service.impl;
 
+import com.rohila.api.bean.Resource;
+import com.rohila.api.controller.UserController;
+import com.rohila.api.gateway.UserGateway;
+import com.rohila.api.response.Response;
+import com.rohila.api.service.UserService;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(MockitoJUnitRunner.class)
 class UserServiceImplTest {
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    @Mock
+    UserGateway userGateway;
 
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    void userAction() {
+    void testUserActionFollow() {
+        Mockito.when(userGateway.follow(Mockito.any(Long.class),Mockito.any(Long.class))).thenReturn("success");
+        Response response = userService.userAction("follows", new Long(1), new Long(4));
+        Assert.assertNotNull(response);
     }
 
     @Test
-    void retrieveNewsfeeds() {
+    void testUserActionUnFollow() {
+        Mockito.when(userGateway.unfollow(Mockito.any(Long.class),Mockito.any(Long.class))).thenReturn("success");
+        Response response = userService.userAction("unfollows", new Long(1), new Long(4));
+        Assert.assertNotNull(response);
     }
 
-    @Test
-    void createUser() {
-    }
 }
